e04b4d35dd9ffe1728d5de0eb958972ebfea19da
Abhi Agarwal <me@abhiagarwal.com>
2016-06-19 16:29:50 -0400

    Fixing API issue

--- web/frontend/app/scripts/services/config.js
+++ web/frontend/app/scripts/services/config.js
@@ -8,7 +8,7 @@ angular.module('newshoundApp')
                     var apiHost = "svc/newshound-api/v1";
                     if ($location.host().indexOf('newsai.org') == -1) {
 //                        apiHost = "http://137.116.116.97:8080/"+ apiHost;
-                        apiHost = "http://newshound1.newsai.org/" + apiHost;
+                        apiHost = "http://newshound1.newsai.org:8080/" + apiHost;
                     }
                     return apiHost;
                 }
