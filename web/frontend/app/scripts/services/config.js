94b872c6d5ed6aaaddbefcc724572aacc6a7877c
Abhi Agarwal <me@abhiagarwal.com>
2016-06-19 16:38:08 -0400

    Switching to 8080

--- web/frontend/app/scripts/services/config.js
+++ web/frontend/app/scripts/services/config.js
@@ -5,12 +5,7 @@ angular.module('newshoundApp')
         function($location) {
             return {
                 apiHost: function() {
-                    var apiHost = "svc/newshound-api/v1";
-                    if ($location.host().indexOf('newsai.org') == -1) {
-//                        apiHost = "http://137.116.116.97:8080/"+ apiHost;
-                        apiHost = "http://newshound1.newsai.org:8080/" + apiHost;
-                    }
-                    return apiHost;
+                    return "http://newshound1.newsai.org:8080/svc/newshound-api/v1";
                 }
             };
         }
